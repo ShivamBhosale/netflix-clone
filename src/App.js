41a19e160766bf39416ad58d3f7d9d0889b33f42
Shivam Sunil Bhosale <shivambhosale1511@gmail.com>
2024-05-19 18:28:21 -0400

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import Row from './Row';
 import Banner from './Banner';
 import Nav from './Nav';
